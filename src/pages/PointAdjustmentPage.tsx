59f96dd3f9e6dbfbe4441fdb84bc52e7a318d06b
misung-dev <misung.dev@gmail.com>
2025-10-04 01:47:05 +0900

    #4 [FEAT] 페이지 라우팅 및 파비콘 적용 (#5)
    * #4 [FEAT] 페이지 라우팅 추가
    
    * #4 [FEAT] 로고 및 이미지 파일 추가
    
    * #4 [FEAT] 필요한 폴더 생성
    
    * #4 [FEAT] 파비콘 적용 및 불필요한 아이콘 삭제

---
+++ src/pages/PointAdjustmentPage.tsx
@@ -0,0 +1,3 @@
+export default function PointAdjustmentPage() {
+  return <div>PointAdjustmentPage</div>;
+}
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as ExamReviewPage } from './ExamReviewPage';
+export { default as MemberInfoPage } from './MemberInfoPage';
+export { default as PointAdjustmentPage } from './PointAdjustmentPage';
+export { default as SignInPage } from './SignInPage';
