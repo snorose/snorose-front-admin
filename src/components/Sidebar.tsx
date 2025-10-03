3de628fb0980009b4e59cdefc81cc7569daec321
misung.dev <misung.dev@gmail.com>
2025-10-04 02:33:41 +0900

    #6 [STYLE] 사이드바에 로고 및 아이콘 추가

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,23 +1,30 @@
 import { Link, useLocation } from 'react-router-dom';
+import { snoroseLogo } from '@/assets';
+import { FaBookOpen, FaMoneyBillWave, FaUserGear } from 'react-icons/fa6';
 
 const Sidebar: React.FC = () => {
   const location = useLocation();
 
-  const menuItems = [
-    { path: '/exam', label: '시험 검토' },
-    { path: '/member', label: '회원 정보' },
-    { path: '/point', label: '포인트 조정' },
+  const MENU_ITEMS = [
+    { path: '/member', label: '회원 정보', icon: <FaUserGear /> },
+    { path: '/exam', label: '시험 후기', icon: <FaBookOpen /> },
+    { path: '/point', label: '포인트 증감', icon: <FaMoneyBillWave /> },
   ];
 
   return (
-    <nav className='bg-blue-100 p-4 w-50'>
-      <ul className='text-left'>
-        {menuItems.map((item) => (
-          <li key={item.path}>
+    <nav className='bg-blue-100 p-4 w-50 flex flex-col gap-4'>
+      <div>
+        <img src={snoroseLogo} alt='logo' />
+      </div>
+
+      <ul className='text-left flex flex-col gap-1'>
+        {MENU_ITEMS.map((item) => (
+          <li key={item.path} className='hover:bg-gray-100 rounded-md p-1 px-3'>
             <Link
               to={item.path}
-              className={`${location.pathname === item.path ? 'active' : ''}`}
+              className={`flex items-center gap-2 ${location.pathname === item.path ? 'active' : ''}`}
             >
+              {item.icon}
               <span>{item.label}</span>
             </Link>
           </li>
