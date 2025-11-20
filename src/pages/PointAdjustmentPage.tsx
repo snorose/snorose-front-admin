4227b06a92a4adc1110915faa71bfada1ab57d74
misung.dev <misung.dev@gmail.com>
2025-11-21 02:03:32 +0900

    #10 [FEAT] 레이아웃 일부 수정

--- src/pages/PointAdjustmentPage.tsx
+++ src/pages/PointAdjustmentPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Button,
   Input,
@@ -9,12 +10,11 @@ import {
 } from '@/components/ui';
 import { MEMBER_SAMPLE_DATA } from '@/__mocks__';
 import type { MemberInfo } from '@/types';
-import { useState } from 'react';
 import { cn } from '@/utils';
 import { POINT_CATEGORY } from '@/constants';
 
 const MEMBER_INFO: { label: string; key: keyof MemberInfo }[] = [
-  { label: '사용자 ID', key: 'userId' },
+  { label: '회원 ID', key: 'userId' },
   { label: '이름', key: 'userName' },
   { label: '아이디', key: 'loginId' },
   { label: '학번', key: 'studentNumber' },
@@ -52,7 +52,7 @@ export default function PointAdjustmentPage() {
   };
 
   return (
-    <div className='flex w-full flex-col gap-4'>
+    <div className='flex w-full flex-col gap-6'>
       <h1 className='text-2xl font-bold'>포인트 증감(지급/차감)</h1>
       <article className='flex flex-col gap-1'>
         <h3 className='text-lg font-bold'>회원 조회</h3>
@@ -62,8 +62,13 @@ export default function PointAdjustmentPage() {
             placeholder='이름, 아이디, 학번 중 하나를 입력해주세요'
             className='w-96'
           />
-          <Button type='submit' className='h-10 w-20 text-black'>
-            <span>검색</span>
+          <Button
+            type='submit'
+            size='sm'
+            variant='outline'
+            className='h-auto w-20 cursor-pointer text-black'
+          >
+            검색
           </Button>
         </div>
         <div className='w-full rounded-md border p-2'>
@@ -127,64 +132,60 @@ export default function PointAdjustmentPage() {
 
       <article className='flex flex-col gap-1'>
         <h3 className='text-lg font-bold'>지급할 포인트 상세</h3>
-        <div className='flex w-full flex-col gap-6 rounded-md border p-4'>
-          <div className='flex gap-4'>
-            <div className='flex w-1/2 flex-col gap-1'>
-              <label htmlFor='userId' className='font-bold'>
-                회원 userId *
-              </label>
-              <Input
-                type='number'
-                id='userId'
-                placeholder='직접 입력 or 검색 후 선택'
-                value={userId}
-                onChange={handleUserIdChange}
-              />
-            </div>
-            <div className='flex w-1/2 flex-col gap-1'>
-              <label htmlFor='difference' className='font-bold'>
-                증감 포인트 *
-              </label>
-              <Input
-                type='number'
-                id='difference'
-                placeholder='증감할 포인트를 입력 (예: 20, -50)'
-              />
-            </div>
+        <div className='grid w-full grid-cols-2 gap-4 rounded-md border p-4'>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='userId' className='font-bold'>
+              회원 ID (userId) *
+            </label>
+            <Input
+              type='number'
+              id='userId'
+              placeholder='직접 입력 or 검색 후 선택'
+              value={userId}
+              onChange={handleUserIdChange}
+            />
           </div>
-          <div className='flex gap-4'>
-            <div className='flex w-1/2 flex-col gap-1'>
-              <label htmlFor='category' className='font-bold'>
-                포인트 유형 *
-              </label>
-              <Select
-                onValueChange={(
-                  selectedKey: keyof typeof POINT_CATEGORY | ''
-                ) => setSelectedCategory(selectedKey)}
-                value={selectedCategory || undefined}
-              >
-                <SelectTrigger className='w-full'>
-                  <SelectValue placeholder='포인트 유형을 선택해주세요' />
-                </SelectTrigger>
-                <SelectContent>
-                  {Object.entries(POINT_CATEGORY).map(([key, label]) => (
-                    <SelectItem key={key} value={key}>
-                      {label}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
-            <div className='flex w-1/2 flex-col gap-1'>
-              <label htmlFor='memo' className='font-bold'>
-                메모
-              </label>
-              <Input
-                type='text'
-                id='memo'
-                placeholder='이벤트 당첨 포인트 지급, 시험 후기 오류 제보 등'
-              />
-            </div>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='difference' className='font-bold'>
+              증감 포인트 *
+            </label>
+            <Input
+              type='number'
+              id='difference'
+              placeholder='증감할 포인트를 입력 (예: 20, -50)'
+            />
+          </div>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='category' className='font-bold'>
+              포인트 유형 *
+            </label>
+            <Select
+              onValueChange={(selectedKey: keyof typeof POINT_CATEGORY | '') =>
+                setSelectedCategory(selectedKey)
+              }
+              value={selectedCategory || undefined}
+            >
+              <SelectTrigger className='w-full'>
+                <SelectValue placeholder='포인트 유형을 선택해주세요' />
+              </SelectTrigger>
+              <SelectContent>
+                {Object.entries(POINT_CATEGORY).map(([key, label]) => (
+                  <SelectItem key={key} value={key}>
+                    {label}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <label htmlFor='memo' className='font-bold'>
+              메모
+            </label>
+            <Input
+              type='text'
+              id='memo'
+              placeholder='이벤트 당첨 포인트 지급, 시험 후기 오류 제보 등'
+            />
           </div>
         </div>
       </article>
