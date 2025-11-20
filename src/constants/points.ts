b800418372bb676cc9af5983d927c4ddd8f3c5a1
misung.dev <misung.dev@gmail.com>
2025-11-21 01:48:51 +0900

    #10 [FEAT] 포인트 enum value 변경

--- src/constants/points.ts
+++ src/constants/points.ts
@@ -5,12 +5,12 @@ export const POINT_CATEGORY = Object.freeze({
     '시험후기 관련 신고 보상 (중복/허위 족보 등)',
   POINT_REWARD_REPORT_OUTSIDER: '외부인 신고 보상',
   POINT_REWARD_REPORT_PERMANENT_DEMOTION:
-    '영구강등에 해당하는 위반사항 신고 보상 (외부 유출, 아이디 공유 등)',
-  POINT_REWARD_ETC: '기타 사유',
+    '영구 강등에 해당하는 위반사항 신고 보상 (외부 유출, 아이디 공유 등)',
+  POINT_REWARD_ETC: '포인트 보상 - 기타 사유',
 
   // 포인트 차감
-  POINT_DEDUCTION_FLOODING: '글/댓글 도배 적발',
-  POINT_DEDUCTION_ETC: '기타 사유',
+  POINT_DEDUCTION_FLOODING: '포인트 차감 - 글/댓글 도배 적발',
+  POINT_DEDUCTION_ETC: '포인트 차감 - 기타 사유',
 
   // 이벤트
   EVENT: '이벤트 참여',
