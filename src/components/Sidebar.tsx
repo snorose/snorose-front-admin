818f0e9c3f4424d8c22b2f2a17e9cadd0ec159c6
misung-dev <misung.dev@gmail.com>
2025-11-23 15:10:25 +0900

    #27 [STYLE] 사이드바 스타일 수정 (#28)
    * #27 [STYLE] 사이드바 섹션 흰 배경으로 수정 및 너비 수정
    
    * #27 [CHORE] lucide icon으로 변경
    
    * #27 [STYLE] 아이콘 교체 및 사이드 메뉴 사이즈 축소

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,33 +1,44 @@
 import { Link, useLocation } from 'react-router-dom';
-import { HiBookOpen, HiChatAlt, HiDocumentText } from 'react-icons/hi';
-import { BiCoinStack } from 'react-icons/bi';
-import { FaUserCog } from 'react-icons/fa';
+import {
+  House,
+  UserCog,
+  BookOpen,
+  CircleParking,
+  FileText,
+  MessageCircle,
+} from 'lucide-react';
 
 const Sidebar: React.FC = () => {
   const location = useLocation();
+  const ICON_SIZE = 16;
 
   const MENU_ITEMS = [
-    { path: '/member', label: '회원 정보', icon: <FaUserCog /> },
-    { path: '/exam', label: '시험 후기', icon: <HiBookOpen /> },
-    { path: '/point', label: '포인트 증감', icon: <BiCoinStack /> },
-    { path: '/post', label: '게시글 관리 (준비중)', icon: <HiDocumentText /> },
+    { path: '/home', label: '홈 (준비중)', icon: House },
+    { path: '/member', label: '회원 정보', icon: UserCog },
+    { path: '/exam', label: '시험 후기', icon: BookOpen },
+    { path: '/point', label: '포인트 증감', icon: CircleParking },
+    {
+      path: '/post',
+      label: '게시글 관리 (준비중)',
+      icon: FileText,
+    },
     {
       path: '/comment',
       label: '댓글 관리 (준비중)',
-      icon: <HiChatAlt />,
+      icon: MessageCircle,
     },
   ];
 
   return (
-    <nav className='p-4 py-6 flex flex-col border-r-1 border-gray-200 bg-gray-100 w-60'>
-      <ul className='text-left flex flex-col gap-4'>
+    <nav className='flex w-50 flex-col border-r-1 border-gray-200 p-4 py-6'>
+      <ul className='flex flex-col gap-4 text-left'>
         {MENU_ITEMS.map((item) => (
           <li
             key={item.path}
-            className={`hover:text-blue-500 text-base ${location.pathname === item.path ? 'text-blue-500 font-bold active' : ''}`}
+            className={`text-sm hover:text-blue-500 ${location.pathname === item.path ? 'active font-bold text-blue-500' : ''}`}
           >
-            <Link to={item.path} className={`flex items-center gap-2 `}>
-              {item.icon}
+            <Link to={item.path} className={`flex items-center gap-2`}>
+              <item.icon size={ICON_SIZE} />
               <span>{item.label}</span>
             </Link>
           </li>
