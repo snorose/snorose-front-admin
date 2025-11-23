e5b67a421cbf884a6dc0da69d9e382c77e21bf96
misung.dev <misung.dev@gmail.com>
2025-11-23 17:17:50 +0900

    #32 [FEAT] 사이드바 열고 닫힘 기능 구현

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import {
   House,
@@ -6,12 +7,16 @@ import {
   CircleParking,
   FileText,
   MessageCircle,
+  PanelLeft,
 } from 'lucide-react';
+import { snoroseLogo } from '@/assets';
+import { cn } from '@/utils';
 
 const Sidebar: React.FC = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const location = useLocation();
-  const ICON_SIZE = 16;
 
+  const ICON_SIZE = 16;
   const MENU_ITEMS = [
     { path: '/home', label: '홈 (준비중)', icon: House },
     { path: '/member', label: '회원 정보', icon: UserCog },
@@ -29,17 +34,49 @@ const Sidebar: React.FC = () => {
     },
   ];
 
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <nav className='flex w-50 flex-col border-r-1 border-gray-200 p-4 py-6'>
+    <nav
+      className={cn(
+        'flex flex-col gap-8 border-r-1 border-gray-200 py-4 transition-all duration-300',
+        isSidebarOpen ? 'w-50' : 'w-auto'
+      )}
+    >
+      <div
+        className={`flex items-center justify-between px-4.5 transition-all duration-300`}
+      >
+        <img
+          src={snoroseLogo}
+          alt='logo'
+          className={cn(
+            'h-5 transition-all duration-300',
+            isSidebarOpen ? 'w-auto opacity-100' : 'hidden opacity-0'
+          )}
+        />
+        <PanelLeft
+          size={ICON_SIZE}
+          className='cursor-pointer'
+          onClick={toggleSidebar}
+        />
+      </div>
       <ul className='flex flex-col gap-4 text-left'>
         {MENU_ITEMS.map((item) => (
           <li
             key={item.path}
             className={`text-sm hover:text-blue-500 ${location.pathname === item.path ? 'active font-bold text-blue-500' : ''}`}
           >
-            <Link to={item.path} className={`flex items-center gap-2`}>
+            <Link to={item.path} className={`flex items-center gap-2 px-4.5`}>
               <item.icon size={ICON_SIZE} />
-              <span>{item.label}</span>
+              <span
+                className={cn(
+                  isSidebarOpen ? 'opacity-100' : 'hidden opacity-0'
+                )}
+              >
+                {item.label}
+              </span>
             </Link>
           </li>
         ))}
