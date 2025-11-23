cf4db14b854964f85d55ad96871894ed0b5b36ee
misung-dev <misung.dev@gmail.com>
2025-11-23 16:32:08 +0900

    #30 [STYLE] 로그인 페이지 수정 (#31)
    * #30 [STYLE] 로그인 페이지 UI 수정
    
    * #30 [STYLE] 로그인 페이지 내 비밀번호 visivility 아이콘 추가
    
    * #30 [STYLE] 로그인 페이지 배경 색상 제거 및 그림자 효과 추가

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,54 +1,75 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Eye, EyeOff } from 'lucide-react';
 import { snoroseLogo } from '@/assets';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { Button, Input } from '@/components/ui';
 
 export default function LogInPage() {
   const navigate = useNavigate();
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     navigate('/member');
   };
 
-  return (
-    <main className='flex flex-col items-center justify-center w-full h-full gap-4'>
-      <div className='w-full p-4 flex justify-center items-center flex-col gap-4'>
-        <img className='w-64' src={snoroseLogo} alt='logo' />
-        <p className='text-lg text-center'>
-          스노로즈 어드민 페이지에 오신 것을 환영합니다.
-        </p>
-      </div>
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
-      <form className='p-4 flex flex-col gap-4 w-80' onSubmit={handleLogin}>
-        <div className='flex flex-col gap-1'>
-          <label htmlFor='id' className='text-lg'>
-            아이디
-          </label>
+  // TODO: 아이디, 비밀번호에 required 추가 (현재 주석 처리해둠)
+  return (
+    <main className='flex min-h-screen items-center justify-center p-4'>
+      <div className='flex flex-col items-center justify-center gap-8 rounded-2xl bg-white px-16 py-20 shadow-[0_-2px_8px_rgba(0,0,0,0.04),0_4px_16px_rgba(0,0,0,0.10)]'>
+        <div className='flex flex-col items-center gap-2'>
+          <img className='h-12 w-auto' src={snoroseLogo} alt='스노로즈 로고' />
+          <p className='text-sm text-gray-600'>
+            어드민 페이지에 오신 것을 환영합니다
+          </p>
+        </div>
+        <form className='flex w-80 flex-col gap-2' onSubmit={handleLogin}>
           <Input
             id='id'
-            placeholder='아이디를 입력하세요'
+            placeholder='스노로즈 아이디'
             name='id'
             type='text'
-            className='h-10'
+            className='h-11 text-base'
+            // required
           />
-        </div>
-        <div className='flex flex-col gap-1'>
-          <label htmlFor='password' className='text-lg'>
-            비밀번호
-          </label>
-          <Input
-            id='password'
-            placeholder='비밀번호를 입력하세요'
-            name='password'
-            type='password'
-            className='h-10'
-          />
-        </div>
-        <Button type='submit' className='text-black h-10 text-lg'>
-          <span>로그인</span>
-        </Button>
-      </form>
+
+          <div className='relative'>
+            <Input
+              id='password'
+              placeholder='스노로즈 비밀번호'
+              name='password'
+              type={showPassword ? 'text' : 'password'}
+              className='h-11 text-base'
+              // required
+            />
+            <button
+              type='button'
+              onClick={togglePasswordVisibility}
+              className='absolute top-1/2 right-3 -translate-y-1/2 text-gray-500 hover:cursor-pointer'
+              aria-label={showPassword ? '비밀번호 숨기기' : '비밀번호 보기'}
+            >
+              {showPassword ? (
+                <Eye className='size-5' />
+              ) : (
+                <EyeOff className='size-5' />
+              )}
+            </button>
+          </div>
+
+          <Button
+            type='submit'
+            size='lg'
+            variant='outline'
+            className='hover: h-11 w-full cursor-pointer text-base'
+          >
+            로그인
+          </Button>
+        </form>
+      </div>
     </main>
   );
 }
