613ea8161e11861ec64e95447b9f6a0a862592f0
misung.dev <misung.dev@gmail.com>
2025-11-26 01:04:36 +0900

    #35 [FEAT] 기본 UI 구현

--- src/pages/PushNotificationPage.tsx
+++ src/pages/PushNotificationPage.tsx
@@ -1,3 +1,108 @@
+import { Button, Input, Textarea } from '@/components/ui';
+
 export default function PushNotificationPage() {
-  return <div>PushNotificationPage</div>;
+  return (
+    <div className='flex w-full flex-col gap-6'>
+      <h1 className='text-2xl font-bold'>푸시 알림 전송</h1>
+      <section className='flex gap-4'>
+        <article className='flex w-full flex-col gap-1'>
+          <h3 className='text-lg font-bold'>필수 정보</h3>
+          <div className='flex w-full flex-col gap-4 rounded-md border p-4'>
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='name' className='font-semibold'>
+                알림명 *
+              </label>
+              <Input
+                type='text'
+                id='name'
+                placeholder='내부 확인 및 구분용 (예: 251013 리뉴얼 1주년 기념 포인트 지급 안내)'
+                // value={userId}
+                // onChange={handleUserIdChange}
+              />
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='title' className='font-semibold'>
+                알림 제목 *
+              </label>
+              <Input
+                type='text'
+                id='title'
+                placeholder='예: 스노로즈 리뉴얼 1주년 기념 포인트'
+                // value={difference}
+                // onChange={(e) => setDifference(Number(e.target.value) || '')}
+              />
+              <div className='flex px-2'>
+                <p className='text-xs text-gray-500'>0 / 21자</p>
+              </div>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='body' className='font-semibold'>
+                알림 내용 *
+              </label>
+              <Textarea
+                id='body'
+                placeholder='예: 모든 정회원 여러분께 10포인트 선물이 도착했습니다! 지급 내역은 [내정보 &gt; 포인트 내역 보기] 에서 확인하실 수 있습니다. (2025.10.12 19시 기준 정회원 대상)'
+                // value={difference}
+                // onChange={(e) => setDifference(Number(e.target.value) || '')}
+              />
+              <div className='flex px-2'>
+                <p className='text-xs text-gray-500'>0 / 100자</p>
+              </div>
+            </div>
+
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='url' className='font-semibold'>
+                알림 클릭 시 연결되는 주소 *
+              </label>
+              <Input
+                type='text'
+                id='url'
+                placeholder='기본 주소의 뒷부분, 기본값: “/” (예: /board/notice/post/1863135)'
+                // value={memo}
+                // onChange={(e) => setMemo(e.target.value)}
+              />
+            </div>
+          </div>
+        </article>
+
+        <article className='flex w-full flex-col gap-1'>
+          <h3 className='text-lg font-bold'>발송 옵션</h3>
+          <div className='flex flex-col gap-4 rounded-md border p-4'>
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='isMarketing' className='font-semibold'>
+                광고성 알림 여부
+              </label>
+            </div>
+            <div className='flex flex-col gap-1'>
+              <label htmlFor='isTest' className='font-semibold'>
+                테스트 발송 여부
+              </label>
+            </div>
+          </div>
+        </article>
+      </section>
+
+      <div className='flex justify-end gap-2'>
+        <Button
+          type='submit'
+          size='lg'
+          variant='outline'
+          // onClick={handleResetButtonClick}
+          className='text-md h-10 w-32 cursor-pointer font-bold text-red-400 hover:text-red-400 active:text-red-600'
+        >
+          초기화
+        </Button>
+        <Button
+          type='submit'
+          size='lg'
+          variant='outline'
+          className='text-md h-10 w-32 cursor-pointer font-bold'
+        >
+          알림 전송
+        </Button>
+      </div>
+    </div>
+  );
 }
