b6cab8aa5e45e2e46bba5914fd7fa744bf895c6b
misung-dev <misung.dev@gmail.com>
2025-11-30 02:55:47 +0900

    #42 [REFACTOR] PageHeader 컴포넌트 생성 및 적용 (#43)
    * #42 [REFACTOR] PageHeader 컴포넌트 생성 및 적용
    
    * #42 [REFACTOR] 시멘틱 태그로 변경

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,13 @@
+interface PageHeaderProps {
+  title: string;
+  description?: string;
+}
+
+export default function PageHeader({ title, description }: PageHeaderProps) {
+  return (
+    <header className='flex flex-col gap-1'>
+      <h1 className='text-2xl font-bold'>{title}</h1>
+      {description && <p className='text-sm text-gray-500'>{description}</p>}
+    </header>
+  );
+}
