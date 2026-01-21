ed0032ee5581fd22b32b82ad3de21223d410742d
misung-dev <misung.dev@gmail.com>
2026-01-22 01:31:38 +0900

    #95 [REFACTOR] 페이지 컴포넌트 이동 및 라우트 복수 표기 (#96)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export { default as CommentPage } from './CommentPage';
+export { default as CommentPage } from './posts/CommentPage';
 export { default as ExamReviewPage } from './ExamReviewPage';
 export { default as MemberInfoPage } from './member/MemberInfoPage';
 export { default as MemberPenaltyManagementPage } from './member/MemberPenaltyManagementPage';
@@ -6,6 +6,6 @@ export { default as AdjustSinglePointPage } from './points/AdjustSinglePointPage
 export { default as PointMultiplePage } from './points/PointMultiplePage';
 export { default as AdjustAllMemberPointPage } from './points/AdjustAllMemberPointPage';
 export { default as PointFreezePage } from './points/PointFreezePage';
-export { default as LogInPage } from './LogInPage';
-export { default as PostPage } from './PostPage';
+export { default as LogInPage } from './login/LogInPage';
+export { default as PostPage } from './posts/PostPage';
 export { default as PushNotificationPage } from './alerts/PushNotificationPage';
