af465fafd6cb8c5b06a9cfbce2780a7c74cac5f5
misung.dev <misung.dev@gmail.com>
2026-01-28 14:45:38 +0900

    [DESIGN] #118 테이블 컬럼 수 변경에 따른 일부 속성 변경

--- src/domains/Reviews/components/ExamTableFallback.tsx
+++ src/domains/Reviews/components/ExamTableFallback.tsx
@@ -17,9 +17,6 @@ export function ExamTableSkeleton({
     <>
       {Array.from({ length: itemsPerPage }).map((_, index) => (
         <TableRow key={`skeleton-${index}`} className='[&_td]:h-[24px]'>
-          <TableCell className='w-[70px] text-center'>
-            <Skeleton className='mx-auto h-4 w-8' />
-          </TableCell>
           <TableCell className='w-[50px]'>
             <Skeleton className='mx-auto h-2 w-2 rounded-full' />
           </TableCell>
@@ -41,9 +38,6 @@ export function ExamTableSkeleton({
           <TableCell className='w-[60px]'>
             <Skeleton className='h-4 w-full' />
           </TableCell>
-          <TableCell className='w-[150px]'>
-            <Skeleton className='h-4 w-full' />
-          </TableCell>
           <TableCell className='w-[110px]'>
             <Skeleton className='h-4 w-full' />
           </TableCell>
@@ -59,7 +53,7 @@ export function ExamTableSkeleton({
 export function ExamTableEmpty() {
   return (
     <TableRow>
-      <TableCell colSpan={11} className='h-[240px] p-0 text-gray-500'>
+      <TableCell colSpan={9} className='h-[240px] p-0 text-gray-500'>
         <div className='flex h-full items-center justify-center'>
           해당하는 데이터가 없습니다
         </div>
@@ -75,9 +69,6 @@ export function ExamTableEmptyRows({ count }: ExamTableEmptyRowsProps) {
     <>
       {Array.from({ length: count }).map((_, index) => (
         <TableRow key={`empty-${index}`} className='[&_td]:h-[24px]'>
-          <TableCell className='w-[70px] text-center text-gray-600'>
-            &nbsp;
-          </TableCell>
           <TableCell className='w-[50px]'>&nbsp;</TableCell>
           <TableCell className='w-[200px]'>&nbsp;</TableCell>
           <TableCell className='w-[120px]'>&nbsp;</TableCell>
@@ -85,7 +76,6 @@ export function ExamTableEmptyRows({ count }: ExamTableEmptyRowsProps) {
           <TableCell className='w-[84px]'>&nbsp;</TableCell>
           <TableCell className='w-[60px]'>&nbsp;</TableCell>
           <TableCell className='w-[60px]'>&nbsp;</TableCell>
-          <TableCell className='w-[150px]'>&nbsp;</TableCell>
           <TableCell className='w-[110px]'>&nbsp;</TableCell>
           <TableCell className='w-[80px]'>&nbsp;</TableCell>
         </TableRow>
