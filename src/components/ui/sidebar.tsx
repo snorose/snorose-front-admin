02cda76241a460d8be641d3618e52ec4c76c20e0
misung.dev <misung.dev@gmail.com>
2026-01-28 15:48:17 +0900

    [REFACTOR] #122 공통 lib를 shared로 이동

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -6,7 +6,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { PanelLeftIcon } from 'lucide-react';
 
 import { useIsMobile } from '@/shared/hooks';
-import { cn } from '@/lib/utils';
+import { cn } from '@/shared/lib';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
