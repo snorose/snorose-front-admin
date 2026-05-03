12d4530efac7c15affc2282ed198f1b21ecd9cb7
misung.dev <misung.dev@gmail.com>
2026-05-03 16:18:32 +0900

    [MODIFY] #209 시험후기 목록 API 경로 수정 및 데이터 표시 방식 반영

--- src/domains/Reviews/types/exam.ts
+++ src/domains/Reviews/types/exam.ts
@@ -29,17 +29,20 @@ export interface ExamReview {
 }
 
 export interface ExamReviews {
-  userDisplay: string;
-  isWriterWithdrawn: boolean;
   postId: number;
-  title: string;
-  questionDetail: string;
-  isConfirmed: boolean;
-  commentCount: number;
-  scrapCount: number;
-  isScrapped: boolean;
-  createdAt: string;
-  isEdited: boolean;
+  status: string;
+  content: string;
+  lecture: string;
+  professor: string;
+  encryptedUserId: string | null;
+  userName: string | null;
+  lectureYear: number;
+  lectureSemester: Semester;
+  examType: ExamType;
+  classNumber: number;
+  contentDate: string;
+  encryptedAdminId: string | null;
+  adminName: string | null;
 }
 
 export interface ConfirmExamReviewRequest {
