de1b9cbf08d1cea07d0b79a9ed11929d3b6d9c40
misung-dev <misung.dev@gmail.com>
2026-05-17 17:26:38 +0900

    [FEAT] #235 게시글 관리 페이지 주석 처리 및 사이드 스타일 변경 (#236)
    * [CHORE] #229 이슈 자동 할당 워크플로우 수정 (#232)
    
    * [FEAT] #235 시험후기 관리 메뉴 이름 수정
    
    * [STYLE] #235 사이드메뉴 스타일 변경
    
    ---------
    
    Co-authored-by: 하지민 <163178666+JiiminHa@users.noreply.github.com>

--- src/shared/constants/sidebar-menus.ts
+++ src/shared/constants/sidebar-menus.ts
@@ -1,7 +1,7 @@
 import {
   // Bell,
   BookOpen,
-  FileText,
+  // FileText,
   HandCoins,
   type LucideIcon,
   User,
@@ -36,23 +36,22 @@ export const SIDEBAR_MENUS: SidebarMenu[] = [
       // },
     ],
   },
-
+  // {
+  //   title: '게시글 관리',
+  //   icon: FileText,
+  //   items: [
+  //     {
+  //       title: '댓글 관리',
+  //       url: PATHS.POST_COMMENTS,
+  //     },
+  //   ],
+  // },
   {
-    title: '게시글 관리',
-    icon: FileText,
-    items: [
-      {
-        title: '댓글 관리',
-        url: PATHS.POST_COMMENTS,
-      },
-    ],
-  },
-  {
-    title: '시험 후기',
+    title: '시험후기',
     icon: BookOpen,
     items: [
       {
-        title: '관리',
+        title: '시험후기 관리',
         url: PATHS.REVIEW_EXAM,
       },
       {
