e6446e88f489eec003bca6aee58bdd4bb18ed48b
misung.dev <misung.dev@gmail.com>
2026-05-24 16:47:22 +0900

    refactor: #237 미사용 팝업 상태 요약 컴포넌트 제거

--- src/domains/Operation/components/index.ts
+++ src/domains/Operation/components/index.ts
@@ -1,4 +1,3 @@
 export { MarkdownPreview } from './MarkdownPreview';
 export { PopupEditorDialog } from './PopupEditorDialog';
 export { PopupManagementTable } from './PopupManagementTable';
-export { PopupStatusSummary } from './PopupStatusSummary';
