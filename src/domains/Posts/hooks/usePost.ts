93381c6dfe14f961d982e058bbbf1ce8cff4ca26
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-06-20 11:24:42 +0900

    [REFACTOR] #267 Query 통합 및 에러 처리 try-catch로 전환

--- src/domains/Posts/hooks/usePost.ts
+++ src/domains/Posts/hooks/usePost.ts
@@ -2,27 +2,22 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getDeletedPost, getPost } from '@/apis';
 
-export const usePost = (postId: number | null, deletedAt?: string | null) => {
-  const firstQuery = useQuery({
+export const usePost = (postId: number | null) => {
+  const query = useQuery({
     queryKey: ['post', postId],
-    queryFn: () => (deletedAt ? getDeletedPost(postId!) : getPost(postId!)),
+    queryFn: async () => {
+      try {
+        return await getPost(postId!);
+      } catch {
+        return await getDeletedPost(postId!);
+      }
+    },
     enabled: !!postId,
-    retry: deletedAt === undefined ? false : undefined,
-  });
-
-  const secondQuery = useQuery({
-    enabled: !!postId && !!firstQuery.error && deletedAt === undefined,
-    queryKey: ['deletedPost', postId],
-    queryFn: () => getDeletedPost(postId!),
   });
 
   return {
-    post: firstQuery.error ? secondQuery.data : firstQuery.data,
-    isLoading: firstQuery.isLoading || secondQuery.isLoading,
-    error: deletedAt === undefined ? secondQuery.error : firstQuery.error,
-    refetch: () => {
-      firstQuery.refetch();
-      secondQuery.refetch();
-    },
+    post: query.data,
+    isLoading: query.isLoading,
+    error: query.error,
   };
 };
