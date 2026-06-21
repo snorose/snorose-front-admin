8f81da743daeb28ccb2c73835f4dc220cb897ae3
misung-dev <misung.dev@gmail.com>
2026-06-21 16:52:42 +0900

    feat: 미지급 일정 목록에 기간 상태 배지 표시 (#299)
    * feat: 미지급 일정 목록에 기간 상태 배지 표시
    
    * test: 테스트 케이스 추가

--- src/domains/Points/components/PointFreezeListSection.tsx
+++ src/domains/Points/components/PointFreezeListSection.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import { MoreHorizontalIcon } from 'lucide-react';
 
+import { PeriodStatusBadge } from '@/shared/components';
 import { Button, DropdownMenu, Table } from '@/shared/components/ui';
 import type { PointFreeze } from '@/shared/types';
 import { formatDateTimeToMinutes } from '@/shared/utils';
@@ -48,6 +49,7 @@ export function PointFreezeListSection({
             <Table.Header>
               <Table.Row className='text-center'>
                 <Table.Head className='text-center'>번호</Table.Head>
+                <Table.Head className='text-center'>상태</Table.Head>
                 <Table.Head className='text-center'>일정 제목</Table.Head>
                 <Table.Head className='text-center'>시작 일시</Table.Head>
                 <Table.Head className='text-center'>종료 일시</Table.Head>
@@ -66,6 +68,9 @@ export function PointFreezeListSection({
                     <Table.Row key={id}>
                       <Table.Cell className='text-center'>
                         {index + 1}
+                      </Table.Cell>
+                      <Table.Cell className='text-center'>
+                        <PeriodStatusBadge startAt={startAt} endAt={endAt} />
                       </Table.Cell>
                       <Table.Cell className='text-center'>{title}</Table.Cell>
                       <Table.Cell className='text-center'>
