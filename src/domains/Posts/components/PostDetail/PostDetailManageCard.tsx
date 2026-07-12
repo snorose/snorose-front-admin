347a03df393ff5fbbe8307a9851a45dbf4638fdd
하지민 <163178666+JiiminHa@users.noreply.github.com>
2026-07-12 18:51:21 +0900

    style: 게시글 삭제 토스트 메시지 종류 수정 (#345)
    * style: 게시글 삭제 토스트 메시지 종류 수정
    
    * chore: 코드 리뷰 수정 사항 반영

--- src/domains/Posts/components/PostDetail/PostDetailManageCard.tsx
+++ src/domains/Posts/components/PostDetail/PostDetailManageCard.tsx
@@ -30,12 +30,16 @@ export default function PostDetailManageCard({
   // 게시물 삭제 Mutation
   const deleteMutation = useMutation({
     mutationFn: (memo: string) => deletePost(post.postId, memo),
-    onSuccess: () => {
-      toast.info(
-        deleteCommentsAlso
-          ? '게시글은 삭제되었지만 댓글 삭제 기능은 개발 중입니다.'
-          : '게시글이 삭제되었습니다.'
-      );
+    onMutate: () => {
+      return { deleteCommentsAlsoAtMutation: deleteCommentsAlso };
+    },
+    onSuccess: (_data, _variables, context) => {
+      if (context?.deleteCommentsAlsoAtMutation) {
+        toast.info('게시글은 삭제되었지만 댓글 삭제 기능은 개발 중입니다.');
+      } else {
+        toast.success('게시글이 삭제되었습니다.');
+      }
+
       setIsModalOpen(false);
       setReason('');
       setDeleteCommentsAlso(false);
