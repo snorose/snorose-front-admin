8eb1d600d7438d6d826c8cf545c67bd534404a7d
misung-dev <misung.dev@gmail.com>
2026-08-24 01:31:42 +0900

    refactor: MemberInfo·공유 모듈 배럴 인덱스 export를 명시적으로 정리

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -8,6 +8,13 @@ export {
 export { downloadNotProcessedRowsExcel } from './download-not-processed-excel';
 export { getErrorMessage } from './error-handler';
 export { getPeriodStatus, type PeriodStatus } from './periodStatusUtils';
+export {
+  BOARD_OPTIONS,
+  formatPostId,
+  getPostStatusBadges,
+  STATUS_OPTIONS,
+  stripHtmlTags,
+} from './postCommentUtils';
 export { cookie, tokenStorage, userStorage } from './storage';
 export { executeTokenRefresh, TokenRefreshManager } from './token-manager';
 export {
